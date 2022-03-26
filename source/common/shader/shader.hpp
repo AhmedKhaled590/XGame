f08b10ae6a76d5e662fd64c7cd13309460357998
Ahmedkhaled590 <kahmd1444@gmail.com>
2022-03-26 16:36:37 +0200

    answerQuestion

--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -61,13 +61,19 @@ namespace our
         }
 
         // TODO: Delete the copy constructor and assignment operator
-        // Question: Why do we do this? Hint: Look at the deconstructor
         ShaderProgram(const ShaderProgram &) = delete;
         ShaderProgram &operator=(const ShaderProgram &) = delete;
 
+        // Question: Why do we do this? Hint: Look at the deconstructor
         /**
          * Answer:
-         * we
+         * The compiler-generated copy constructor is wrong; it copies the OpenGL object name, not the OpenGL object itself.
+         * This leaves two C++ objects which each intend to destroy the same OpenGL object.
+         * Ideally, leacing copy constructor will allow copy of the OpenGL object's data into a new OpenGL object. 
+         * This would leave each C++ object with its own unique OpenGL object.
+         *  However, copying an OpenGL object's data to a new object is incredibly expensive; 
+         * it is also essentially impossible to do. So instead, we should forbid copying of OpenGL wrapper objects. 
+         * Resources like shaderprograms shouldn't be copyable, but instead moveable.
          */
     };
 
