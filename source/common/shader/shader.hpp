99a98b895a2face788025253c84f77ca31bc3ff3
Ahmedkhaled590 <kahmd1444@gmail.com>
2022-03-28 18:14:29 +0200

    answer Q

--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -69,10 +69,10 @@ namespace our
          * Answer:
          * The compiler-generated copy constructor is wrong; it copies the OpenGL object name, not the OpenGL object itself.
          * This leaves two C++ objects which each intend to destroy the same OpenGL object.
-         * Ideally, leacing copy constructor will allow copy of the OpenGL object's data into a new OpenGL object. 
+         * Ideally, leacing copy constructor will allow copy of the OpenGL object's data into a new OpenGL object.
          * This would leave each C++ object with its own unique OpenGL object.
-         *  However, copying an OpenGL object's data to a new object is incredibly expensive; 
-         * it is also essentially impossible to do. So instead, we should forbid copying of OpenGL wrapper objects. 
+         *  However, copying an OpenGL object's data to a new object is incredibly expensive;
+         * it is also essentially impossible to do. So instead, we should forbid copying of OpenGL wrapper objects.
          * Resources like shaderprograms shouldn't be copyable, but instead moveable.
          */
     };
